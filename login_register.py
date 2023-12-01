160f593db1b25d72e304cc65559decaaa4485435
Rayan <Rayan.d15@outlook.com>
2023-12-01 16:02:54 +0100

    Fixed Small Problem in login function

--- login_register.py
+++ login_register.py
@@ -43,7 +43,7 @@ def is_logged_in():
         data = cursor.fetchone()
         return session['user_id'], data[1], data[2], data[3], data[4], data[5], data[6], data[7]
     else:
-        return 'None'
+        return None
     
 def does_mail_already_exist(email, namn, telefonnummer, password):
     # Does the informaiton already exist?
