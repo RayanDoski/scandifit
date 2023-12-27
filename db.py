c95a787fa28f51897e5bc5fc8c546c53d198d462
Rayan <Rayan.d15@outlook.com>
2023-12-27 22:15:18 +0100

    Making Changes To Database, mkaing it normal again

--- db.py
+++ db.py
@@ -1,18 +1,12 @@
 import mysql.connector
-def establish_database_connection():
-    while True:
-        try:
-            db = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                passwd="Rayan12345",
-                database="scandifit",
-            )
-            return db
-        except:
-            print("CONNECTION FAILED")
 
-db = establish_database_connection()
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="Rayan12345",
+    database="scandifit",
+)
+
 cursor = db.cursor()
 
 from flask_mail import Mail
