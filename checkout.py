bffa7c2fdaf188d368f7566111c106406dfc194a
Rayan <Rayan.d15@outlook.com>
2023-12-31 22:41:23 +0100

    Added try exept to delete from cart

--- checkout.py
+++ checkout.py
@@ -4,19 +4,22 @@ checkout = Blueprint('checkout', __name__)
 
 @checkout.route('/delete-from-cart')
 def delete_item():
-    item_id = request.args.get('id')
+    try:
+        item_id = request.args.get('id')
 
-    if 'product-id' in session:
-        product_ids = session['product-id']
+        if 'product-id' in session:
+            product_ids = session['product-id']
 
-        if int(item_id) in session['product-id']:
-            product_ids.remove(int(item_id))
-            session['product-id'] = product_ids
+            if int(item_id) in session['product-id']:
+                product_ids.remove(int(item_id))
+                session['product-id'] = product_ids
 
-        if not session['product-id']:
-            session.pop('product-id')
+            if not session['product-id']:
+                session.pop('product-id')
+                return redirect(request.referrer)
+            
             return redirect(request.referrer)
-        
+    except:
         return redirect(request.referrer)
 
 @checkout.route('/add-product', methods=['post', 'get'])
